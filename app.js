e23d3a895c232209464f67b1f2647712ee7c27f8
Secfu <secfu1603@gmail.com>
2023-12-11 21:33:48 -0600

    proyect 4

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
+// // app.js
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const listViewRouter = require("./list-view-router");
+// const listEditRouter = require("./list-edit-router");
+
+// const app = express();
+// const port = 3000;
+
+// // Middleware a nivel de aplicación para gestionar métodos HTTP no válidos
+// const handleInvalidMethod = (req, res, next) => {
+//   if (!["GET", "POST", "PUT", "DELETE"].includes(req.method)) {
+//     return res.status(400).json({ error: "Método HTTP no válido." });
+//   }
+//   next();
+// };
+
+// app.use(bodyParser.json());
+// app.use(handleInvalidMethod);
+
+// // Monta los routers en rutas específicas
+// app.use("/tasks/view", listViewRouter);
+// app.use("/tasks/edit", listEditRouter);
+
+// app.listen(port, () => {
+//   console.log(`La aplicación está escuchando en http://localhost:${port}`);
+// });
+
 // app.js
 const express = require("express");
 const bodyParser = require("body-parser");
-const listViewRouter = require("./list-view-router");
-const listEditRouter = require("./list-edit-router");
+const dotenv = require("dotenv");
+const authRouter = require("./auth-router");
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
-// Middleware a nivel de aplicación para gestionar métodos HTTP no válidos
-const handleInvalidMethod = (req, res, next) => {
-  if (!["GET", "POST", "PUT", "DELETE"].includes(req.method)) {
-    return res.status(400).json({ error: "Método HTTP no válido." });
-  }
-  next();
-};
-
 app.use(bodyParser.json());
-app.use(handleInvalidMethod);
 
-// Monta los routers en rutas específicas
-app.use("/tasks/view", listViewRouter);
-app.use("/tasks/edit", listEditRouter);
+// Monta el router de autenticación en la ruta /auth
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
